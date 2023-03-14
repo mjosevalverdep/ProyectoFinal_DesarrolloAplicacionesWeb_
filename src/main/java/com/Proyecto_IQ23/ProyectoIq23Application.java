f208042fe3ccb5f41119643fcc29fe13ff9c9f8f
María José Valverde Peña <mariajosevalverdepena@gmail.com>
2023-03-14 00:31:11 -0600

    Primer commit #1 proyecto

---
+++ src/main/java/com/Proyecto_IQ23/ProyectoIq23Application.java
@@ -0,0 +1,13 @@
+package com.Proyecto_IQ23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIq23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIq23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto_IQ23/ProyectoIq23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto_IQ23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIq23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
