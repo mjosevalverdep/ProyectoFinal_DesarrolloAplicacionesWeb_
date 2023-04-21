e2709349b733cfe2265fbfa68462a0770e48bb85
JeanBarahonaG <122959383+JeanBarahonaG@users.noreply.github.com>
2023-04-21 08:46:50 -0600

    Modificacion del securityconfig, index Se añadio, webconfig y errores 403 y pagina de login

--- src/main/java/com/Proyecto_IQ23/SecurityConfig.java
+++ src/main/java/com/Proyecto_IQ23/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.Proyecto_IQ23;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -21,4 +22,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("USER");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/tour/nuevoTour", "/tour/guardarTour",
+                        "/tour/modificarTour/**", "/tour/eliminarTour/**",
+                        "/oferta/nuevoOferta", "/oferta/guardarOferta",
+                        "/oferta/modificarOferta/**", "/oferta/eliminarOferta/**")
+                .hasRole("ADMIN")
+                .antMatchers("/oferta/listadoOferta", "/tour/listadoTour","/Destinos",
+                        "/About","/Contacto")
+                .hasAnyRole("ADMIN", "USER")
+                .antMatchers("/")
+                .hasAnyRole("ADMIN", "USER")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .exceptionHandling().accessDeniedPage("/errores/403");
+    }
 }
